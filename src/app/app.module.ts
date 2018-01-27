1f6096109c54f702f5e8b9a4c8e954e0c164d9e3
peishanwang <wang.peish@husky.neu.edu>
2018-01-27 01:02:18 -0800

    change local directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './views/users/login/login.component';
+import { ProfileComponent } from './views/users/profile/profile.component';
+import { RegisterComponent } from './views/users/register/register.component';
+import { FormsModule } from '@angular/forms';
+import {routing} from './app.routing';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProfileComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
